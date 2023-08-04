4b5865f0b2dccd9b0db0a0c95c644484aab14021
Marcelo Suarez <amsuarez99@gmail.com>
2023-08-04 00:30:12 -0600

    Button.tsx

---
+++ packages/core/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+type ButtonProps = PropsWithChildren<{
+  /**
+   * What happens when you click the button
+   * @returns {void}
+   */
+  onClick: () => void;
+}>;
+export function Button(props: ButtonProps) {
+  return <button onClick={props.onClick}>Other button</button>;
+}
